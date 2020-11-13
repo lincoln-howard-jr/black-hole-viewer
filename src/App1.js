182f28f485cededa8c4b160bd99233d4bfc348d5
larastars <lara.kamal10@gmail.com>
2020-11-13 16:23:58 -0500

    modifying the z value

--- src/App1.js
+++ src/App1.js
@@ -36,10 +36,10 @@ class App extends Component {
     let zeros = 9 - selection[0].length
     // debugger
     let file = "grid" + "0".repeat(zeros) + selection[0]
-    if(selection[2] === 1) {
-      file += "_Z_1_n_"
-    } else {
+    if(selection[2] === 0.1) {
       file += "_Z_0p1_n_"
+    } else {
+      file += "_Z_1_n_"
     }
     file += selection[3] + "_" + selection[1] + "per.con"
     // Set configuration file to state
